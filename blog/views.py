9143011590ecc4f724a9970fc61f193cba0e5426
Fdrv <oldgot@gmail.com>
2021-05-06 18:07:29 +0300

    extract get_likes_count func out

--- blog/views.py
+++ blog/views.py
@@ -26,10 +26,13 @@ def serialize_tag(tag):
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
+def get_likes_count(post):
+    return post.likes.count()
 
 def index(request):
     most_popular_posts = sorted(Post.objects.all(),
-                                key=lambda post: -post.likes.count())[:5]
+                                key=get_likes_count,
+                                reverse=True)[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
